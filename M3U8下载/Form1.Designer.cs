ed0b9554a9adc0c4365065294ac73de2995e1299
yajusenpai <yanruoshiichi@qq.com>
2020-12-02 23:19:44 +0800

    Add files via upload

---
+++ M3U8下载/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace M3U8下载
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "main.m3u8地址：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "保存路径";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(4, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(253, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(17, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "导出名称";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(4, 157);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(253, 26);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "export_file";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(258, 243);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "基本信息";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(4, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 47);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "识别";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 253);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(258, 113);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "选择分辨率";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(246, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(245, 40);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "下载";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 373);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(257, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 403);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(257, 46);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "退出程序";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 461);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9.163636F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 505);
+            this.MinimumSize = new System.Drawing.Size(300, 505);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "M3U8下载器";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ M3U8下载/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace M3U8下载
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        string savepath = "", exp_name, url, target_m3u8, domain, URL_PATH;
+        HttpDldFile downloader = new HttpDldFile();
+        Dictionary<string, string> r2url = new Dictionary<string, string>();//分辨率对应的集合
+        Thread downT;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        List<string> t_list = new List<string>();
+
+        void DownloadT()
+        {
+            target_m3u8 = r2url[comboBox1.Text];
+            Uri downtargeturl = new Uri("http://" + domain + "/" + target_m3u8);
+            downloader.Download(downtargeturl.ToString(), savepath + "\\temp.m3u8");
+            string tmp = File.ReadAllText(savepath + "\\temp.m3u8");
+            URL_PATH = downtargeturl.ToString();
+            for (int i = URL_PATH.Length - 1; i >= 0; i--)
+            {
+                if (URL_PATH[i] == '/')
+                {
+                    break;
+                }
+                URL_PATH = URL_PATH.Substring(0, URL_PATH.Length - 1);
+            }
+            foreach (var i in tmp.Split('\n')) if (i.Length > 0) if (i[0] != '#') t_list.Add(i);
+            string savetmp = savepath + "\\temp.t";
+            string fsave = savepath + "\\" + exp_name;
+            FileStream fs = new FileStream(fsave, FileMode.OpenOrCreate);
+
+            int index = 0;
+            foreach (var i in t_list)
+            {
+                progressBar1.Value = Convert.ToInt32(((double)((double)index / (double)t_list.Count)) * 100);
+                downtargeturl = new Uri(URL_PATH + "/" + i);
+                downloader.Download(downtargeturl.ToString(), savetmp);
+                byte[] vs = File.ReadAllBytes(savetmp);
+                long clength = fs.Length;
+                fs.Position = clength;
+                fs.Write(vs, 0, vs.Length);
+                File.Delete(savetmp);
+                index++;
+            }
+            fs.Close();
+            MessageBox.Show("下载完毕", "M3U8下载器");
+            Thread.CurrentThread.Abort();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            downT = new Thread(DownloadT);
+            downT.IsBackground = true;
+            downT.Start();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog vs = new FolderBrowserDialog();
+            if (vs.ShowDialog() == DialogResult.OK)
+            {
+                savepath = vs.SelectedPath;
+                textBox2.Text = savepath;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t_list.Clear();
+            exp_name = textBox3.Text;
+            url = textBox1.Text;
+            Uri u = new Uri(url);
+            domain = u.Host;
+            if (string.IsNullOrWhiteSpace(savepath) || string.IsNullOrWhiteSpace(exp_name))
+            {
+                MessageBox.Show("信息完整", "M3U8下载器"); return;
+            }
+            downloader.Download(url, savepath + "\\main.m3u8");
+            if (!File.Exists(savepath + "\\main.m3u8"))
+            {
+                MessageBox.Show("无法下载m3u8文件", "M3U8下载器"); return;
+            }
+            /*
+#EXTM3U
+#EXT-X-STREAM-INF:PROGRAM-ID=1, BANDWIDTH=460800, RESOLUTION=480x270
+/asp/hls/450/0303000a/3/default/d528201c9276bcef23a58f3be1937d9f/450.m3u8
+#EXT-X-STREAM-INF:PROGRAM-ID=1, BANDWIDTH=870400, RESOLUTION=480x270
+/asp/hls/850/0303000a/3/default/d528201c9276bcef23a58f3be1937d9f/850.m3u8
+#EXT-X-STREAM-INF:PROGRAM-ID=1, BANDWIDTH=1228800, RESOLUTION=1280x720
+/asp/hls/1200/0303000a/3/default/d528201c9276bcef23a58f3be1937d9f/1200.m3u8
+             */
+            string mainm = File.ReadAllText(savepath + "\\main.m3u8");
+            for (int i = 0; i < mainm.Split('\n').Length; i++)
+            {
+                var source = mainm.Split('\n');
+                var array = mainm.Split('\n')[i];
+                if (array.Contains("RESOLUTION"))
+                {
+                    int index = 0;
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        if (array[j] == 'N' && array[j - 1] == 'O')
+                        {
+                            index = j + 2; break;
+                        }
+                    }
+                    try
+                    {
+                        r2url.Add(array.Substring(index), mainm.Split('\n')[i + 1]);
+                        comboBox1.Items.Add(array.Substring(index));
+                    }
+                    catch { }
+                }
+            }
+            comboBox1.SelectedItem = comboBox1.Items[comboBox1.Items.Count - 1];
+            File.Delete(savepath + "\\main.m3u8");
+        }
+    }
+}
